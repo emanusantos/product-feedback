0a75c4e97dba929758c1df2e2183261b2786b27f
emanusantos <emanuel.lmds1@gmail.com>
2023-07-26 21:10:49 -0300

    feat: setup header component (missing bulb icon and select)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { HomeComponent } from './routes/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
     HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
