b4c0080be27e1e0dc3d2b4779eb89f83fff08971
emanusantos <emanuel.lmds1@gmail.com>
2023-07-29 19:30:15 -0300

    feat: cursors and hovers

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.sass']
+  styleUrls: ['./button.component.sass'],
 })
 export class ButtonComponent {
   @Input() label = '';
