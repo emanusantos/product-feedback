0252f2055bbe44f6b31811d7487c7ef820da243e
emanusantos <emanuel.lmds1@gmail.com>
2023-07-28 23:57:10 -0300

    feat: finish aside menu

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -3,14 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.sass']
+  styleUrls: ['./select.component.sass'],
 })
 export class SelectComponent {
   initialValue = 'downvotes';
 
   getSelectedValue(event: any) {
     this.initialValue = event.target.value;
-
-    console.log(this.initialValue)
   }
 }
