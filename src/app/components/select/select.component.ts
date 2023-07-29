0afe6eb30dc9130df05758e4b3f474f6644d48ef
emanusantos <emanuel.lmds1@gmail.com>
2023-07-29 16:04:34 -0300

    feat: select styling

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 
+type Option = {
+  label: string;
+  value: 'upvotes' | 'comments';
+  order: 'ASC' | 'DESC';
+};
+
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.sass'],
 })
 export class SelectComponent {
-  initialValue = 'downvotes';
+  isSelectVisible = false;
 
-  getSelectedValue(event: any) {
-    this.initialValue = event.target.value;
+  options: Array<Option> = [
+    { label: 'Most Upvotes', value: 'upvotes', order: 'DESC' },
+    { label: 'Least Upvotes', value: 'upvotes', order: 'ASC' },
+    { label: 'Most Comments', value: 'comments', order: 'DESC' },
+    { label: 'Least Comments', value: 'comments', order: 'ASC' },
+  ];
+
+  selectedOption: Option = this.options[0];
+
+  toggleSelect() {
+    this.isSelectVisible = !this.isSelectVisible;
+  }
+
+  selectOption(option: Option) {
+    this.selectedOption = option;
+
+    this.isSelectVisible = false;
+  }
+
+  getIcon() {
+    return this.isSelectVisible
+      ? '../../../assets/shared/icon-arrow-up.svg'
+      : '../../../assets/shared/icon-arrow-down.svg';
   }
 }
