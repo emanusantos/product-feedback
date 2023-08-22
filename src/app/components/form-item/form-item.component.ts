c2c4b2f5a52c6166e5735e56bbfcf9cf53ae9e1e
emanusantos <emanuel.lmds1@gmail.com>
2023-08-21 21:33:12 -0300

    feat: make title and description dynamic on form item component

--- src/app/components/form-item/form-item.component.ts
+++ src/app/components/form-item/form-item.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form-item',
   templateUrl: './form-item.component.html',
-  styleUrls: ['./form-item.component.sass']
+  styleUrls: ['./form-item.component.sass'],
 })
 export class FormItemComponent {
-
+  @Input() title = '';
+  @Input() description = '';
 }
